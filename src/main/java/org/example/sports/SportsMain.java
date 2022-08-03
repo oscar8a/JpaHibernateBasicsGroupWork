5438ae54fffef586f27b2f8a6e44b4ed15e9f248
Moe <m.ibrahim0228@gmail.com>
2022-08-03 14:51:38 -0400

    functionality works; need to add ability for user to choose sport and other statistics;

--- src/main/java/org/example/sports/SportsMain.java
+++ src/main/java/org/example/sports/SportsMain.java
@@ -7,10 +7,12 @@ import org.example.shared.io.console.ConsoleUserOutputServiceImpl;
 import org.example.shared.io.validation.NonBlankInputValidationRule;
 import org.example.sports.model.Statistic;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SportsMain {
     public static void main(String[] args) throws Exception {
@@ -43,32 +45,64 @@ public class SportsMain {
                         transaction.begin();
                         entityManager.persist(statistic);
                         transaction.commit();
+                        userChoice = getUserChoice(userInputService);
                         break;
                     case 2:
                         transaction.begin();
-                        Statistic statistic1= getDistinctPlayerForNewStatistic(userInputService,entityManager);
+                        Statistic statistic1 = getDistinctPlayerForNewStatistic(userInputService, entityManager);
                         Statistic anotherStatistic = addStatisticToPlayer(statistic1, entityManager, userInputService);
                         entityManager.persist(anotherStatistic);
                         transaction.commit();
-
-
-                        break;
-                    case 3:
-                        break;
-                    default:
                         userChoice = getUserChoice(userInputService);
                         break;
-//                    case 3: int userStatisticChoice = getUserStatisticChoice(userInputService);
-//                    switch(userStatisticChoice){
-//                        case 1:
+                    case 3:
+                        int userStatisticChoice = getUserStatisticChoice(userInputService);
+                        switch (userStatisticChoice) {
+                            case 1:
+                                Statistic statisticToGetAVG = getDistinctPlayerForNewStatistic(userInputService, entityManager);
+                                String query = "SELECT S from Statistic S where S.name = '" + statisticToGetAVG.getName() + "'";
+                                List<Statistic> statisticsToAverage = entityManager.createQuery(query,
+                                        Statistic.class).getResultList();
+
+                                float average = 0;
+                                double count = 0;
+                                double total = 0;
+                                for (Statistic stat : statisticsToAverage) {
+                                    total += stat.getScoreInGame();
+                                    count++;
+                                }
+                                average = (float) (total / count);
+                                System.out.println("Average score= " + average);
+                                break;
+                            case 2:
+                                Statistic statisticToGetMax = getDistinctPlayerForNewStatistic(userInputService, entityManager);
+                                String queryMax = "SELECT S from Statistic S where S.name = '" + statisticToGetMax.getName() + "'";
+                                List<Statistic> statisticsToMax = entityManager.createQuery(queryMax,
+                                        Statistic.class).getResultList();
+                                int max = 0;
+                                for (Statistic stat : statisticsToMax) {
+                                    if (max < stat.getScoreInGame()) {
+                                        max = stat.getScoreInGame();
+                                    }
+                                }
+                                System.out.println("Max points= " + max);
+                                break;
+                            default:
+                                break;
+                        }
+                        userChoice = getUserChoice(userInputService);
+                        break;
+                    case 4:
+                        runValue = false;
+                        break;
+                    default:
+                        System.out.println("Please choose a number from the selection given");
+                        break;
                 }
+
             }
+
         }
-
-
-//            Statistic statistic = entityManager.find(Statistic.class, 1);
-//            entityManager.remove(statistic);
-
     }
 
 
@@ -83,7 +117,8 @@ public class SportsMain {
         return Integer.parseInt(userInputService.getUserInput("What would you like to do?\n" +
                         "1. Add an athlete with a game statistic\n" +
                         "2. Add another game statistic for the same athlete\n" +
-                        "3. Get statistics information on that athlete",
+                        "3. Get statistics information on that athlete\n" +
+                        "4. Exit program.",
                 new NonBlankInputValidationRule()));
 
     }
@@ -101,13 +136,15 @@ public class SportsMain {
 
 
     public static Statistic getDistinctPlayerForNewStatistic(UserInputService userInputService, EntityManager entityManager) {
-        String hql = "SELECT Distinct S FROM Statistic S";
+        String hql = "SELECT S from Statistic S";
 
         List<Statistic> statisticNamesList = entityManager.createQuery(hql,
-                    Statistic.class).getResultList();
+                Statistic.class).getResultList();
         List<String> playerNames = new ArrayList<>();
         for (int i = 0; i < statisticNamesList.size(); i++) {
-            playerNames.add(statisticNamesList.get(i).getName());
+            if (!playerNames.contains(statisticNamesList.get(i).getName())) {
+                playerNames.add(statisticNamesList.get(i).getName());
+            }
         }
         String playerNamesString = "";
         int count = 0;
